d95ee4b71e82107e61d4824968d2698a2a97a9e9
Max <max.arvid.anderson@gmail.com>
2018-08-21 16:34:37 -0400

    add twitter unfollower

---
+++ keys_to_be_filled_out.py
@@ -0,0 +1,7 @@
+keys = dict(
+    screen_name = [''], #Twitter handle goes here
+    consumer_key =          'XXXXXXXX',
+    consumer_secret =       'XXXXXXXX',
+    access_token =          'XXXXXXXX',
+    access_token_secret =   'XXXXXXXX'
+)
